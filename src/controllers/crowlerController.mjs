5bb053264b520754ea078d999657da82625152de
ThiagoArcangelo <devtsarcangelo@gmail.com>
2023-02-06 15:10:14 -0300

    Get Data Scraping

--- src/controllers/crowlerController.mjs
+++ src/controllers/crowlerController.mjs
@@ -1,55 +1,56 @@
-import puppeteer from 'puppeteer';
+import pup from "puppeteer";
 
 export default async function listCrowler(req, res) {
   async function webCrowlerVagas() {
-    const browser = await puppeteer.launch({
+    const browser = await pup.launch({
       // headless: false,
       // slowMo: 150
     });
-  const page = await browser.newPage();
+    const page = await browser.newPage();
 
-  const url = "https://www.bauruempregos.com.br/home/vagas";
+    const url = "https://www.bauruempregos.com.br/home/vagas";
 
- await page.goto(url);
+    await page.goto(url);
 
-  try {
+    try {
+      await page.waitForSelector("div.vaga");
 
-    await page.waitForSelector(".vaga > .descricao-vaga > a ");
+      // const vaga = await page.$eval('.descricao-vaga > a ', (element) => element.innerText);
+      // const clickUrl = await page.$eval(
+      //   ".descricao-vaga > a ",
+      //   (element) => element.href
+      // );
+      // const local = await page.$eval('.cidade-vaga', (element) => element.textContent);
 
-    // const vaga = await page.$eval('.descricao-vaga > a ', (element) => element.innerText);
-    // const local = await page.$eval('.cidade-vaga', (element) => element.textContent);
-    
-    const result = await page.evaluate(() => {
-    
-      const contentArray = document.querySelectorAll('div.vaga');
+      const result = await page.evaluate(() => {
+        const resultArray = document.querySelectorAll("div.vaga");
 
-      Array.from(contentArray).map((element) => {
-          const vaga = element.querySelector('.descricao-vaga').innerText;
-          const local = element.querySelector('.cidade-vaga').textContent;
+        const contentData = Array.from(resultArray).map((element) => {
+          const vaga = element.querySelector(".descricao-vaga").innerText;
+          const local = element.querySelector(".cidade-vaga").textContent;
+          const clickUrl = element.getElementsByTagName("a");
 
-          // const data = {Vaga: vaga, Local: local};
+          const data = { Vaga: vaga, Local: local, Url: clickUrl };
 
-          // const myData = Array.from(data);
+          return data;
+        });
 
-          return Object.values(vaga, local);
-      })
+        return contentData;
+      });
 
-       return contentArray;
+      // const objLike = { Data: result, Link: clickUrl };
 
-    });
+      // res.json(objLike);
+      res.json(result);
 
-    res.send(result);
-  
-    await browser.close();
-    } 
-    catch (error) {
-      console.log(error) ;
+      await browser.close();
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  webCrowlerVagas(); 
+  webCrowlerVagas();
 }
 
 // Exemplo acertivo no frontend
 // Array.from(result).map((element) => { let data = element.querySelector('.descricao-vaga').innerText; let resultado = Object.values(data); console.log(resultado.join(" "))});
-
