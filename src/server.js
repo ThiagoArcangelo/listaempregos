f745fac2cf16aebad26b2d613dc4f1cf6cf4745a
ThiagoArcangelo <devtsarcangelo@gmail.com>
2024-10-14 23:54:36 -0300

    fix: Remove a função node-cron

--- src/server.js
+++ src/server.js
@@ -1,29 +1,19 @@
 import express from "express";
 import eventEmmiter from "events";
 import axios from "axios";
-import cron from "node-cron";
 import cors from "cors";
-// import router from "./routes/getRoutes.js";
 import router from "./routes/index.js";
-// import EventEmmiter from "events";
 
 import conexaoDb from "./db/db.js";
 
 const app = express();
 const port = process.env.PORT;
-// const emitter = new EventEmmiter();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
 const evento = new eventEmmiter();
-
-cron.schedule('* * 13 * * 1-5 ', () => {
-  evento.on('open', () => {
-    axios.get(`http://localhost:${port}`);
-  });
-});
 
 app.use(router);
 conexaoDb();
