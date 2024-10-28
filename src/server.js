00d2171905e25d5273a9b156c6be81ecb9b9af87
ThiagoArcangelo <devtsarcangelo@gmail.com>
2024-10-27 23:00:22 -0300

    fix: Ajusta a configuração do Cors

--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.urlencoded({extended: true}));
 
 const evento = new eventEmmiter();
